77b11e325f81e761de2fc817101d9d04f0e7b26d
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-08 08:24:30 +0530

    Word and Counter Project added to git. This is an Android Project. In this i have written code that can count the number of words and characters in the given sentence within the edit text. This project is developed using Java Language.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.differentandroidcodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText enterText;
+    TextView characterText;
+    int numberText;
+    String[] words;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterText = findViewById(R.id.textEnter);
+        characterText = findViewById(R.id.characterCounter);
+
+
+        enterText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s == null){
+                    characterText.setText("Character Count :  0\\n\\n\\nWord Count :  0");
+                }
+                else{
+                    numberText = s.length();
+
+                    words = s.toString().split("\\s");
+
+                    characterText.setText("Character Count :  "+numberText+"\n\n\nWord Count :  "+words.length);
+                }
+
+            }
+        });
+
+    }
+}
